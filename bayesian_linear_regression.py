b7984b4a37e1efd09b7ae7f2fdce45dce0a8ce18
Dimitrije Markovic <dimitrije.markovic@tu-dresden.de>
2020-03-10 17:37:46 +0100

    updated bayesian linear regression

--- bayesian_linear_regression.py
+++ bayesian_linear_regression.py
@@ -10,9 +10,34 @@ import numpyro as npyro
 
 import numpyro.distributions as dist
 from numpyro.infer import MCMC, NUTS, log_likelihood, Predictive
+from numpyro.distributions.transforms import Transform, AffineTransform, ComposeTransform
+from numpyro.distributions import constraints
 
 vdot = vmap(np.dot)
 vinv = vmap(np.linalg.inv)
+
+
+class RTransform(Transform):
+    def __init__(self, R, R_inv, domain=constraints.real):
+        self.R = R
+        self.R_inv = R_inv
+
+    @property
+    def codomain(self):
+        return constraints.real
+
+    @property
+    def event_dim(self):
+        return 0
+
+    def __call__(self, x):
+        return vdot(self.R, x)
+
+    def inv(self, y):
+        return vdot(self.R_inv, y)
+
+    def log_abs_det_jacobian(self, x, y, intermediates=None):
+        return np.log(np.abs(np.linalg.det(self.R)))
 
 
 class BayesLinRegress(object):
@@ -30,7 +55,7 @@ class BayesLinRegress(object):
 
         self.rng_key = random.PRNGKey(time())
 
-    def model(self):
+    def model_tmp(self):
         nf = self.nf
         ns = self.ns
 
@@ -53,6 +78,31 @@ class BayesLinRegress(object):
 
         mu = vdot(self.Q, theta)
 
+        npyro.sample('obs', dist.Normal(mu, np.expand_dims(sigma, -1)), obs=self.y_data)
+
+    def model(self):
+        nf = self.nf
+        ns = self.ns
+
+        s = npyro.sample('s', dist.Exponential(np.ones(nf)))
+        m = npyro.sample('m', dist.Normal(np.zeros(nf), 1.))
+
+        sigma = npyro.sample('sigma', dist.InverseGamma(2.*np.ones((ns,)), 1.))
+        tau = npyro.sample('tau', dist.Exponential(10. * np.ones(ns)))
+
+        with npyro.plate('facts', nf):
+            with npyro.plate('subs', ns):
+                lam = npyro.sample('lam', dist.HalfCauchy(1.))
+                z = dist.Normal(0., 1.)
+                rt = RTransform(self.R, self.R_inv)
+                afft = AffineTransform(m * s, np.expand_dims(tau, -1) * lam)
+                cmp_trns = ComposeTransform([afft, rt])
+                theta = npyro.sample('theta', dist.TransformedDistribution(z, cmp_trns))
+
+        npyro.deterministic('group_beta', m * s)
+        npyro.deterministic('beta', rt.inv(theta))
+
+        mu = vdot(self.Q, theta)
         npyro.sample('obs', dist.Normal(mu, np.expand_dims(sigma, -1)), obs=self.y_data)
 
     def fit(self, num_samples=2000, warmup_steps=2000, num_chains=1, summary=True):
