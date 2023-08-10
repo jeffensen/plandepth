54580a36a6567a894600a70d94b62affd88e6d5d
Lorenz Gönner <lorenz.goenner@tu-dresden.de>
2023-08-10 11:40:51 +0200

    Working on cross-fitting simulations (current state: Full planning with and without alpha) Adding model with full planning incl. alpha and hyperbolic discounting

---
+++ calc_BIC.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  8 15:58:30 2023
+
+@author: goenner
+"""
+
+import numpy as np
+
+def calc_BIC(inferrer, responses, conditions, m_prob):
+    # Evaluate (log-)likelihood of actual choices, given the inferred model parameters:    
+    n_subj = len(responses)
+    n_miniblocks = responses.shape[1]
+    nll_firstaction_mean = np.nan * np.ones([n_subj, n_miniblocks])    
+    nll_firstaction_depths = np.nan * np.ones([n_subj, n_miniblocks, 3])
+    nll_hinoise_all = np.nan * np.ones([n_subj, 3])
+    nll_lonoise_all = np.nan * np.ones([n_subj, 3])
+    nll_all = np.nan * np.ones([n_subj, 3])
+    nll_hinoise_120 = np.nan * np.ones([n_subj, 3])
+    nll_lonoise_120 = np.nan * np.ones([n_subj, 3])
+    nll_120 = np.nan * np.ones([n_subj, 3])
+    nll_hinoise_120_mean = np.nan * np.ones([n_subj])
+    nll_lonoise_120_mean = np.nan * np.ones([n_subj])
+    nll_120_mean = np.nan * np.ones([n_subj])
+    pseudo_rsquare_120_mean = np.nan * np.ones([n_subj])
+    pseudo_rsquare_hinoise_120_mean = np.nan * np.ones([n_subj])
+    pseudo_rsquare_lonoise_120_mean = np.nan * np.ones([n_subj])
+    #m_param_count = len(np.unique(pars_df_ya['parameter'])) #  WRONG! Correct solution:  m_param_count = inferrer.agent.np
+    m_param_count = inferrer.agent.np
+    BIC_120_mean = np.nan * np.ones([n_subj])
+    BIC_hinoise_120 = np.nan * np.ones([n_subj])
+    BIC_lonoise_120 = np.nan * np.ones([n_subj])
+
+
+    for i_mblk in range(n_miniblocks):
+        for i_action in range(1): # Consider only first action; use range(3) to consider all action steps
+            #resp = inferrer.responses[0][i_mblk].numpy()[i_action]      # Index 0 to get rid of "duplicate" data
+            #logits_depths = inferrer.agent.logits[3*i_mblk + i_action].detach().numpy()[0] # agent.logits = Value difference between Jump and Move            
+            # CAUTION: If there are as many logit values as particles (e.g., 100), we have to extract the mean rather than index 0!
+            logits_depths = inferrer.agent.logits[3*i_mblk + i_action].detach().numpy() #.mean(0) # agent.logits = Value difference between Jump and Move             
+        
+            for i_subj in range(n_subj):
+                resp = inferrer.responses[i_subj][i_mblk].numpy()[i_action] 
+
+                p_jump_depths = 1.0 / (1 + np.exp(-logits_depths[i_subj])) # softmax function for choice probabilities
+                p_move_depths = 1 - p_jump_depths
+                if resp==1:
+                    nll = -np.log(p_jump_depths) # Apply -np.log() to obtain the negative log-likelihood (nll), for numerical reasons
+                elif resp==0:
+                    nll = -np.log(p_move_depths)                              
+                nll_firstaction_depths[i_subj, i_mblk, :] = nll              
+                #nll_firstaction_mean[i_subj, i_mblk] = np.matmul(nll_firstaction_depths[i_subj, i_mblk, :], m_prob[0][i_mblk,0,:]) # WRONG - m_prob_ya[0] has shape (n_miniblocks, n_subjects, n_steps !!!
+                nll_firstaction_mean[i_subj, i_mblk] = np.matmul(nll_firstaction_depths[i_subj, i_mblk, :], m_prob[0][i_mblk, i_subj, :])            
+
+                nll_hinoise_all[i_subj, :] = np.matmul(nll_firstaction_depths[i_subj, :, :].transpose(), conditions[0][i_subj, :].numpy()) # Sum of NLL for high noise (noise==1) 
+                nll_lonoise_all[i_subj, :] = np.matmul(nll_firstaction_depths[i_subj, :, :].transpose(), 1 - conditions[0][i_subj, :].numpy()) # Sum of NLL for low noise (noise==0)
+                nll_all[i_subj, :] = nll_hinoise_all[i_subj, :] + nll_lonoise_all[i_subj, :]
+                nll_hinoise_120[i_subj, :] = np.matmul(nll_firstaction_depths[i_subj, 20:, :].transpose(), conditions[0][i_subj, 20:].numpy()) # Exclude 20 training miniblocks
+                nll_lonoise_120[i_subj, :] = np.matmul(nll_firstaction_depths[i_subj, 20:, :].transpose(), 1 - conditions[0][i_subj, 20:].numpy()) 
+                nll_120[i_subj, :] = nll_hinoise_120[i_subj, :] + nll_lonoise_120[i_subj, :]
+
+                nll_hinoise_120_mean[i_subj] = np.matmul(nll_firstaction_mean[i_subj, 20:].transpose(), conditions[0][i_subj, 20:].numpy()) # Exclude 20 training miniblocks
+                nll_lonoise_120_mean[i_subj] = np.matmul(nll_firstaction_mean[i_subj, 20:].transpose(), 1 - conditions[0][i_subj, 20:].numpy()) 
+                nll_120_mean[i_subj] = nll_hinoise_120_mean[i_subj] + nll_lonoise_120_mean[i_subj]
+
+                nll_random_120 = -np.log(0.5)*120
+                nll_random_60 = -np.log(0.5)*60    
+                pseudo_rsquare_120_mean[i_subj] = 1 - (nll_120_mean[i_subj] / nll_random_120)
+                pseudo_rsquare_hinoise_120_mean[i_subj] = 1 - (nll_hinoise_120_mean[i_subj] / nll_random_60)    
+                pseudo_rsquare_lonoise_120_mean[i_subj] = 1 - (nll_lonoise_120_mean[i_subj] / nll_random_60)        
+
+                BIC_120_mean[i_subj] = 2*nll_120_mean[i_subj] + m_param_count*np.log(120)
+                BIC_hinoise_120[i_subj] = 2*nll_hinoise_120_mean[i_subj] + m_param_count*np.log(60)
+                BIC_lonoise_120[i_subj] = 2*nll_lonoise_120_mean[i_subj] + m_param_count*np.log(60)    
+                
+    return pseudo_rsquare_120_mean, BIC_120_mean, \
+           pseudo_rsquare_hinoise_120_mean, BIC_hinoise_120, \
+           pseudo_rsquare_lonoise_120_mean, BIC_lonoise_120
+           
--- simulate_agents.py
+++ simulate_agents.py
@@ -30,6 +30,7 @@ import pandas as pd
 sys.path.append('../')
 from tasks import SpaceAdventure
 from agents import BackInduction
+from agents_nonlearning_hilonoise import BackInductionNoLearning
 from agents_discount_Noise_theta import BackInductionDiscountNoiseTheta
 from agents_anchor_pruning import BackInductionAnchorPruning
 from agents_discount_Noise_theta_learnprobs import BackInductionDiscountNoiseThetaLearnprobs
@@ -38,6 +39,9 @@ from agents_discount_Noise_theta_fitprobs import BackInductionDiscountNoiseTheta
 from agents_discount_Noise_theta_realprobs import BackInductionDiscountNoiseThetaRealprobs
 from simulate import Simulator
 from inference import Inferrer
+from helper_files import get_posterior_stats # load_and_format_behavioural_data
+
+from calc_BIC import calc_BIC
 
 # In[1]:  #### simulation and recovery for SAT PD version 2.0 ###########################################
 # changes in task: new planet configs, no action costs, 140 trials in total, first 20 are training trials
@@ -53,7 +57,7 @@ pyro.enable_validation(True)
 
 sns.set(context='talk', style='white', color_codes=True)
 
-runs0 = 10 # 1000 # 40            #number of simulations 
+runs0 = 20 # 1000 # 40            #number of simulations 
 mini_blocks0 = 120     #+20 for training which will be removed in the following
 max_trials0 = 3        #maximum number of actions per mini-block
 max_depth0 = 3         #maximum planning depth
@@ -135,6 +139,7 @@ conditions0[1] = torch.tensor(trials0, dtype=torch.long)
 
 simulations = {}
 simulations['rational'] = []
+simulations['nolearning_hilonoise'] = []
 simulations['discount_noise_theta_gamma0.7'] = []
 simulations['discount_noise_theta_gamma0.3'] = []
 simulations['anchor_pruning'] = []
@@ -147,6 +152,7 @@ simulations['discount_noise_theta_realprobs_gamma0.3'] = []
 
 performance = {}
 performance['rational'] = []
+performance['nolearning_hilonoise'] = []
 performance['discount_noise_theta_gamma0.7'] = []
 performance['discount_noise_theta_gamma0.3'] = []
 performance['anchor_pruning'] = []
@@ -160,6 +166,7 @@ performance['discount_noise_theta_realprobs_gamma0.3'] = []
 
 trans_pars_depth = {}
 trans_pars_depth['rational'] = []
+trans_pars_depth['nolearning_hilonoise'] = []
 trans_pars_depth['discount_noise_theta_gamma0.7'] = []
 trans_pars_depth['discount_noise_theta_gamma0.3'] = []
 trans_pars_depth['anchor_pruning'] = []
@@ -172,6 +179,7 @@ trans_pars_depth['discount_noise_theta_realprobs_gamma0.3'] = []
 
 points_depth = {}
 points_depth['rational'] = []
+points_depth['nolearning_hilonoise'] = []
 points_depth['discount_noise_theta_gamma0.7'] = []
 points_depth['discount_noise_theta_gamma0.3'] = []
 points_depth['anchor_pruning'] = []
@@ -184,6 +192,7 @@ points_depth['discount_noise_theta_realprobs_gamma0.3'] = []
 
 responses_depth = {}
 responses_depth['rational'] = []
+responses_depth['nolearning_hilonoise'] = []
 responses_depth['discount_noise_theta_gamma0.7'] = []
 responses_depth['discount_noise_theta_gamma0.3'] = []
 responses_depth['anchor_pruning'] = []
@@ -196,6 +205,7 @@ responses_depth['discount_noise_theta_realprobs_gamma0.3'] = []
 
 final_points = {}
 final_points['rational'] = []
+final_points['nolearning_hilonoise'] = []
 final_points['discount_noise_theta_gamma0.7'] = []
 final_points['discount_noise_theta_gamma0.3'] = []
 final_points['anchor_pruning'] = []
@@ -208,6 +218,7 @@ final_points['discount_noise_theta_realprobs_gamma0.3'] = []
 
 states = {}
 states['rational'] = []
+states['nolearning_hilonoise'] = []
 states['discount_noise_theta_gamma0.7'] = []
 states['discount_noise_theta_gamma0.3'] = []
 states['anchor_pruning'] = []
@@ -225,16 +236,18 @@ agents = {}
 m0 = {} # mean parameter values
 trans_pars0 = {}
 
-#'''#
-for agent_key in ['rational', 'discount_noise_theta_gamma0.7', 'discount_noise_theta_gamma0.3', \
+'''#
+agent_keys = ['rational', 'nolearning_hilonoise', 'discount_noise_theta_gamma0.7', 'discount_noise_theta_gamma0.3', \
                   'anchor_pruning', 'discount_noise_theta_learnprobs', 'discount_noise_theta_anchor_pruning', \
                   'discount_noise_theta_fitprobs', 'random',  \
                   'discount_noise_theta_realprobs_gamma0.7', 'discount_noise_theta_realprobs_gamma0.3']:
-#'''
+'''
     
-#for agent_key in ['discount_noise_theta_fitprobs']:    
+#agent_keys = ['discount_noise_theta_fitprobs']:    
     
-#for agent_key in ['rational']:    
+agent_keys = ['rational', 'nolearning_hilonoise']   
+    
+for agent_key in agent_keys:    
 
     for i in range(3):
     # define space adventure task with aquired configurations
@@ -258,6 +271,18 @@ for agent_key in ['rational', 'discount_noise_theta_gamma0.7', 'discount_noise_t
             # set beta, theta and alpha parameters as a normal distribution around a certain value
             #m0['rational'] = torch.tensor([1.099, 0., 0.0])# beta= 3, because 1.099=np.log(3); alpha = 0.5 (too high!!!)
             m0['rational'] = torch.tensor([1.099, 0., -2.])#            
+
+        elif agent_key == 'nolearning_hilonoise':    
+            agents['nolearning_hilonoise'] = BackInductionNoLearning(confs0,
+                          runs=runs0,
+                          mini_blocks=mini_blocks0,
+                          trials=3,
+                          costs = torch.tensor([0., 0.]), 
+                          planning_depth=i+1)
+
+            # set beta, theta and alpha parameters as a normal distribution around a certain value
+            #m0['rational'] = torch.tensor([1.099, 0., 0.0])# beta= 3, because 1.099=np.log(3);
+            m0['nolearning_hilonoise'] = torch.tensor([1.099, 0.])#     
             
         elif agent_key == 'discount_noise_theta_gamma0.7': 
             agents['discount_noise_theta_gamma0.7'] = BackInductionDiscountNoiseTheta(confs0,
@@ -412,10 +437,7 @@ for agent_key in ['rational', 'discount_noise_theta_gamma0.7', 'discount_noise_t
 
 # Question:
     # How similar are the actions predicted by the different agent models?
-agent_keys = ['rational', 'discount_noise_theta_gamma0.7', 'discount_noise_theta_gamma0.3', \
-                  'anchor_pruning', 'discount_noise_theta_learnprobs', 'discount_noise_theta_anchor_pruning', \
-                  'discount_noise_theta_fitprobs', 'random',  \
-                  'discount_noise_theta_realprobs_gamma0.7', 'discount_noise_theta_realprobs_gamma0.3']
+
 corr_pvals = np.nan * np.ones(len(agent_keys))
 corr_rhovals = np.nan * np.ones(len(agent_keys))
 
@@ -515,13 +537,165 @@ plt.savefig(datapath+'/Distribution_agents_points.png', bbox_inches='tight', dpi
 sim_number0 = 2
 # This is where inference starts
 
-n_iter = 10 # 100
+n_iter = 1000 # 100
 
 agent2_rational = {}
 agent2_anchor_pruning = {}
 agent2_discount_noise_theta_gamma07 = {}
 
-for simulated_agent_key in ['rational', 'anchor_pruning', 'discount_noise_theta_gamma0.7']: # , 
+fitting_agents = {}
+elbo = {}
+modelfit = {}
+
+datapath = 'P:/037/B3_BEHAVIORAL_STUDY/04_Experiment/Analysis_Scripts/SAT_Results/Results_crossfitting'
+
+for i_fitted in range(len(agent_keys)):
+    fitting_agents['fit-'+agent_keys[i_fitted]] = {}
+    elbo['fit-'+agent_keys[i_fitted]] = {}    
+    modelfit['fit-'+agent_keys[i_fitted]] = {}     
+   
+    for i_simulated in range(len(agent_keys)):    
+        print('fitted: '+agent_keys[i_fitted] + ', simulated: '+agent_keys[i_simulated]) 
+        
+        fitting_agents['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ] = 'fit-'+str(i_fitted)+'-sim-'+str(i_simulated)
+        elbo['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ] = 'fit-'+str(i_fitted)+'-sim-'+str(i_simulated)        
+        modelfit['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ] = {}
+
+        responses0 = simulations[agent_keys[i_simulated]][sim_number0].responses.clone()
+        mask0 = ~torch.isnan(responses0)
+
+        stimuli0 = {'conditions': conditions0,
+           'states': states[agent_keys[i_simulated]][sim_number0],
+           'configs': confs0}            
+
+        if agent_keys[i_fitted] == 'rational':    
+        
+            fitting_agents['fit-rational']['sim-'+agent_keys[i_simulated]] = BackInduction(confs0,
+                          runs=runs0,
+                          mini_blocks=mini_blocks0,
+                          trials=3,
+                          costs = torch.tensor([0., 0.]), 
+                          planning_depth=i+1)
+
+        elif agent_keys[i_fitted] == 'nolearning_hilonoise':    
+            fitting_agents['fit-nolearning_hilonoise']['sim-'+agent_keys[i_simulated]] = BackInductionNoLearning(confs0,
+                          runs=runs0,
+                          mini_blocks=mini_blocks0,
+                          trials=3,
+                          costs = torch.tensor([0., 0.]), 
+                          planning_depth=i+1)    
+
+        
+        infer = Inferrer( fitting_agents['fit-'+agent_keys[i_fitted]]['sim-'+agent_keys[i_simulated]], stimuli0, responses0, mask0)
+        #infer.fit(num_iterations=2000, num_particles=100)#, optim_kwargs={'lr': 0.5}) # 500  # change the number of iterations here if you like
+        infer.fit(num_iterations = n_iter, num_particles=100, optim_kwargs={'lr': 0.1})#) # 500  # change the number of iterations here if you like
+    
+        elbo['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ] = infer.loss[:]
+        n_samples = 100 # 100
+        post_marg = infer.sample_posterior_marginal(n_samples=n_samples)        
+        post_depth, m_prob, exc_count = get_posterior_stats(post_marg)
+        np.savez(datapath + '/plandepth_stats_fit-'+agent_keys[i_fitted]+'_sim-'+agent_keys[i_simulated]+'_'+str(runs0)+'runs_'+str(n_iter)+'iter', post_depth, m_prob, exc_count)
+        
+        pseudo_rsquare_120_mean, BIC_120_mean, \
+           pseudo_rsquare_hinoise_120_mean, BIC_hinoise_120, \
+           pseudo_rsquare_lonoise_120_mean, BIC_lonoise_120 = calc_BIC(infer, responses0, conditions0, m_prob)
+        modelfit['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ]['pseudo_rsquare_120_mean'] = pseudo_rsquare_120_mean
+        modelfit['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ]['pseudo_rsquare_hinoise_120_mean'] = pseudo_rsquare_hinoise_120_mean        
+        modelfit['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ]['pseudo_rsquare_lonoise_120_mean'] = pseudo_rsquare_lonoise_120_mean
+        modelfit['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ]['BIC_120_mean'] = BIC_120_mean        
+        modelfit['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ]['BIC_hinoise_120'] = BIC_hinoise_120                
+        modelfit['fit-'+agent_keys[i_fitted]][ 'sim-'+agent_keys[i_simulated] ]['BIC_lonoise_120'] = BIC_lonoise_120    
+        
+        
+        
+#np.savez(datapath + '/modelfit_fit-'+agent_keys[i_fitted]+'_sim-'+agent_keys[i_simulated], modelfit)
+np.savez(datapath + '/modelfit-default-vs_nolearning_'+str(runs0)+'runs_'+str(n_iter)+'iter_'+str(n_samples)+'samp', modelfit)
+np.savez(datapath + '/elbo-default-vs_nolearning_'+str(runs0)+'runs_'+str(n_iter)+'iter_'+str(n_samples)+'samp', elbo)
+np.savez(datapath + '/responses_depth-default-vs_nolearning_'+str(runs0)+'runs_'+str(n_iter)+'iter_'+str(n_samples)+'samp', responses_depth)
+np.savez(datapath + '/states-default-vs_nolearning_'+str(runs0)+'runs_'+str(n_iter)+'iter_'+str(n_samples)+'samp', states)
+
+# file = np.load(datapath + '/modelfit_fit-'+agent_keys[i_fitted]+'_sim-'+agent_keys[i_simulated], allow_pickle=True)
+
+df_modelfit = pd.DataFrame(modelfit)
+#pd.DataFrame(modelfit).to_csv(datapath + '/modelfit.csv')           
+
+# Confusion matrix: See Wilson & Collins 2019, eLife
+confusion_mat_BIC = np.nan * np.ones([len(agent_keys), len(agent_keys)]) # p(fit | sim) - Probability that a given fitted model fits a given simulated model best, based on BIC
+inversion_mat_BIC = np.nan * np.ones([len(agent_keys), len(agent_keys)]) # p(sim | fit) - Confidence that data resulted from a given simulated model, given a certain best-fitting model based on BIC
+
+confusion_mat_rhosq = np.nan * np.ones([len(agent_keys), len(agent_keys)]) # p(fit | sim) - Probability that a given fitted model fits a given simulated model best, based on rhosquare
+inversion_mat_rhosq = np.nan * np.ones([len(agent_keys), len(agent_keys)]) # p(sim | fit) - Confidence that data resulted from a given simulated model, given a certain best-fitting model based on rhosquare
+
+for i_simulated in range(len(agent_keys)):
+    
+    if i_simulated == 0:
+        sim_model = 'sim-rational'
+    elif i_simulated == 1:
+        sim_model = 'sim-nolearning_hilonoise'        
+        
+    for i_fitted in range(len(agent_keys)):
+        
+        if i_fitted == 0:
+            fit_model = 'fit-rational'
+            other_model = 'fit-nolearning_hilonoise'
+        elif i_fitted == 1:
+            fit_model = 'fit-nolearning_hilonoise'            
+            other_model = 'fit-rational'            
+            
+            confusion_mat_BIC[i_simulated, i_fitted] = \
+                sum(modelfit[fit_model][sim_model]['BIC_120_mean'] < \
+                    modelfit[other_model][sim_model]['BIC_120_mean']) / \
+                    float( runs0 - np.sum(np.isnan(modelfit[other_model][sim_model]['BIC_120_mean'] + modelfit[fit_model][sim_model]['BIC_120_mean']))) # Compare "current" fitted model with other fitted models (per simulated agent)
+
+            confusion_mat_rhosq[i_simulated, i_fitted] = \
+                sum(modelfit[fit_model][sim_model]['pseudo_rsquare_120_mean'] > \
+                    modelfit[other_model][sim_model]['pseudo_rsquare_120_mean']) / \
+                    float( runs0 - np.sum(np.isnan(modelfit[other_model][sim_model]['BIC_120_mean'] + modelfit[fit_model][sim_model]['BIC_120_mean']))) # Compare "current" fitted model with other fitted models (per simulated agent)
+
+
+for i_fitted in range(len(agent_keys)):    
+    for i_simulated in range(len(agent_keys)):    
+        inversion_mat_BIC[i_simulated, i_fitted] = confusion_mat_BIC[i_simulated, i_fitted] / np.sum(confusion_mat_BIC[:, i_fitted])
+        inversion_mat_rhosq[i_simulated, i_fitted] = confusion_mat_rhosq[i_simulated, i_fitted] / np.sum(confusion_mat_rhosq[:, i_fitted])        
+
+
+plt.matshow(confusion_mat_BIC, cmap='viridis', fignum=None) # 'YlOrRd') #
+for i in range(2):
+    for j in range(2):
+        if confusion_mat_BIC[i,j] >= 0.5:
+            plt.text(j, i, str(np.round(confusion_mat_BIC[i,j], 2)), fontsize=20)
+        else:
+            plt.text(j, i, str(np.round(confusion_mat_BIC[i,j], 2)), fontsize=20, color='white')        
+plt.xlabel('best-fitting model')            
+plt.ylabel('simulated model')   
+plt.title('Confusion matrix based on BIC: \n  p(best-fitting model | simulated model)')       
+ax=plt.gca()
+ax.set_xticklabels(['', 'Full planning \n with alpha', 'Full planning \n no alpha'])
+ax.set_yticklabels(['', 'Full planning \n with alpha', 'Full planning \n no alpha'])
+#plt.tight_layout()
+plt.gcf().set_size_inches(10, 7)
+plt.savefig(datapath + '/confusion_mat_BIC_default-vs_nolearning_'+str(runs0)+'runs_'+str(n_iter)+'iter_'+str(n_samples)+'samp.png', dpi=150)
+
+plt.matshow(inversion_mat_BIC, cmap='viridis') # 'YlOrRd') #
+#plt.colorbar()
+for i in range(2):
+    for j in range(2):
+        if inversion_mat_BIC[i,j] >= 0.5:
+            plt.text(j, i, str(np.round(inversion_mat_BIC[i,j], 2)), fontsize=20)
+        else:
+            plt.text(j, i, str(np.round(inversion_mat_BIC[i,j], 2)), fontsize=20, color='white')        
+plt.xlabel('best-fitting model')            
+plt.ylabel('simulated model')   
+plt.title('Inversion matrix based on BIC: \n  p(simulated model | best-fitting model)')       
+ax=plt.gca()
+ax.set_xticklabels(['', 'Full planning \n with alpha', 'Full planning \n no alpha'])
+ax.set_yticklabels(['', 'Full planning \n with alpha', 'Full planning \n no alpha'])
+plt.gcf().set_size_inches(10, 7)
+plt.savefig(datapath + '/inversion_mat_BIC_default-vs_nolearning_'+str(runs0)+'runs_'+str(n_iter)+'iter_'+str(n_samples)+'samp.png', dpi=150)
+
+        
+'''#
+for simulated_agent_key in agent_keys: # , 
     
     responses0 = simulations[simulated_agent_key][sim_number0].responses.clone()
     mask0 = ~torch.isnan(responses0)
@@ -596,7 +770,7 @@ for simulated_agent_key in ['rational', 'anchor_pruning', 'discount_noise_theta_
                           costs = torch.tensor([0., 0.]), 
                           planning_depth=i+1)           
 
-    for fitting_agent_key in ['rational', 'anchor_pruning', 'discount_noise_theta_gamma0.7']:
+    for fitting_agent_key in agent_keys:
         if fitting_agent_key == 'rational':
             infer = Inferrer(agent2_rational[simulated_agent_key], stimuli0, responses0, mask0)
             #infer.fit(num_iterations=2000, num_particles=100)#, optim_kwargs={'lr': 0.5}) # 500  # change the number of iterations here if you like
@@ -611,7 +785,7 @@ for simulated_agent_key in ['rational', 'anchor_pruning', 'discount_noise_theta_
             infer = Inferrer(agent2_discount_noise_theta_gamma07[simulated_agent_key], stimuli0, responses0, mask0)
             #infer.fit(num_iterations=2000, num_particles=100)#, optim_kwargs={'lr': 0.5}) # 500  # change the number of iterations here if you like
             infer.fit(num_iterations = n_iter, num_particles=100, optim_kwargs={'lr': 0.1})#) # 500  # change the number of iterations here if you like
-
+'''
             
         # plotting the ELBO for the given inference 
         plt.figure()
@@ -623,3 +797,4 @@ for simulated_agent_key in ['rational', 'anchor_pruning', 'discount_noise_theta_
         plt.savefig('ELBO_PD3_sim_' + simulated_agent_key + '_fit_' + fitting_agent_key + '.png', dpi=600, bbox_inches='tight')
 
         labels = [r'$\tilde{\beta}$', r'$\theta$',   r'$\tilde{\alpha}$'] #these refer to the next plots and calculations
+        
